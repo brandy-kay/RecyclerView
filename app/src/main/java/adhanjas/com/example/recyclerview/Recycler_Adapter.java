47ff1582fe2b946585fdd492868cafa4e7f322bc
brandy-kay <brandyariana99@gmail.com>
2021-01-23 07:48:32 +0300

    initial commit

---
+++ app/src/main/java/adhanjas/com/example/recyclerview/Recycler_Adapter.java
@@ -0,0 +1,13 @@
+package adhanjas.com.example.recyclerview;
+
+public class Recycler_Adapter {
+    private int image;
+    private String text_Title,text_message;
+
+    public Recycler_Adapter(int image, String text_title, String text_message) {
+        this.image = image;
+        text_Title = text_title;
+        this.text_message = text_message;
+    }
+
+}
